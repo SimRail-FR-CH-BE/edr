f0a08c9e85b5368c96d04994fd6551625ba3949b
MrQuentinH <pro@mrquentin.fr>
2023-11-21 16:03:31 +0100

    set

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,51 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
+  env: {
+    node: true,
+  },
+  extends: [
+    '@vue/typescript/recommended',
     'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    'plugin:vue/vue3-essential',
+    'prettier',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'no-console': [
+      process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+      {
+        allow: ['error'],
+      },
+    ],
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'vue/no-v-html': 'off',
+    'prettier/prettier': [
+      1,
+      {
+        singleQuote: true,
+        semi: true,
+        trailingComma: 'all',
+      },
+    ],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    semi: 'error',
+    quotes: ['error', 'single'],
+    'no-var': 'error',
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+    'no-multi-spaces': 'error',
+    'space-in-parens': 'error',
+    'prefer-const': 'error',
+    'no-use-before-define': 'error',
+    eqeqeq: 'error',
+    curly: ['error'],
+  },
+};
